614ad76d48ba2e96d1451ed101a4bebbf124f4cf
rb3z3rra <sbezerra.rodrigo@gmail.com>
2023-04-10 20:28:51 -0400

    Feat: created Usuario model

---
+++ src/model/Usuario.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const usuarioSchema = new mongoose.Schema({
+  nome: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  senha: { type: String, required: true },
+  image: { type: String, required: true },
+  enderecos: [
+    {
+      rua: { type: String, required: true },
+      numero: { type: Number, required: true },
+      complemento: { type: String },
+      CEP: {type: String, required: true},
+      createdAt: { type: Date, required: true, default: Date.now() }, // Armazena a data de criação dos ende... para futuras auditorias
+    },
+  ],
+  sabores_fav: [
+    {
+      _id: {
+        type: mongoose.Schema.Types.ObjectId,
+        unique: true,
+        ref: "sabores",
+      },
+      createdAt: { type: Date, default: Date.now() },
+    },
+  ],
+  createdAt: { type: Date, default: Date.now() },
+  admin: { type: Boolean, required: true, default: false}, //false será valor padrão
+});
+
+
+const Usuario = mongoose.model("usuarios", usuarioSchema);
+
+module.exports = Usuario;
