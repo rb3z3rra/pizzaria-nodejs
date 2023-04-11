b624d6bf5c251b1c9152267209032b7b21078461
rb3z3rra <sbezerra.rodrigo@gmail.com>
2023-04-10 20:49:18 -0400

    Fix: posicao de chamada ao banco.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const connectToDatabase = require("./src/database/database");
 
+connectToDatabase();
+
+const usuario = require("./src/router/usuario.router");
+
 const app = express();
 
 const porta = 3000;
@@ -8,7 +12,8 @@ const porta = 3000;
 
 app.use(express.json());
 
-connectToDatabase();
+app.use("/usuario", usuario); // Chama as rotas de usuario
+
 
 app.listen(porta, () => {
   console.log(` Servidor rodando em: http://localhost:${porta}`);
