3f2cc23f9a15a409ac8892af5b2d1cfbd50e7228
rb3z3rra <sbezerra.rodrigo@gmail.com>
2023-04-11 19:22:30 -0400

    Feat: install bcrypt e encriptacao de senha

--- src/model/Usuario.js
+++ src/model/Usuario.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const usuarioSchema = new mongoose.Schema({
   nome: { type: String, required: true },
@@ -10,7 +11,7 @@ const usuarioSchema = new mongoose.Schema({
       rua: { type: String, required: true },
       numero: { type: Number, required: true },
       complemento: { type: String },
-      CEP: {type: String, required: true},
+      CEP: { type: String, required: true },
       createdAt: { type: Date, required: true, default: Date.now() }, // Armazena a data de criação dos ende... para futuras auditorias
     },
   ],
@@ -25,9 +26,23 @@ const usuarioSchema = new mongoose.Schema({
   //   },
   // ],
   createdAt: { type: Date, default: Date.now() },
-  admin: { type: Boolean, required: true, default: false}, //false será valor padrão
+  admin: { type: Boolean, required: true, default: false }, //false será valor padrão
 });
 
+//midleware  a ser executado antes de "salvar"
+usuarioSchema.pre("save", async function (next) {
+  if (this.senha) {
+    this.senha = await bcrypt.hash(this.senha, 10); //10 é a quantidade de saltos(quantidade de x embaralhada)
+    next();
+  }
+});
+
+//midleware  a ser executado antes de "atualizar" os dados
+usuarioSchema.pre("findOneAndUpdate", async function (next) {
+  if (this._update.senha) {
+    this._update.senha = await bcrypt.hash(this._update.senha, 10);
+  }
+});
 
 const Usuario = mongoose.model("usuarios", usuarioSchema);
 
