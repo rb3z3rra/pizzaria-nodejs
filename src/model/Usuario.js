b9ef7f7a05d4aee834a8b71faa200b2024e9902c
rb3z3rra <sbezerra.rodrigo@gmail.com>
2023-04-10 22:18:15 -0400

    Fix: remove adress e saboresFav do model

--- src/model/Usuario.js
+++ src/model/Usuario.js
@@ -5,25 +5,25 @@ const usuarioSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   senha: { type: String, required: true },
   image: { type: String, required: true },
-  enderecos: [
-    {
-      rua: { type: String, required: true },
-      numero: { type: Number, required: true },
-      complemento: { type: String },
-      CEP: {type: String, required: true},
-      createdAt: { type: Date, required: true, default: Date.now() }, // Armazena a data de criação dos ende... para futuras auditorias
-    },
-  ],
-  sabores_fav: [
-    {
-      _id: {
-        type: mongoose.Schema.Types.ObjectId,
-        unique: true,
-        ref: "sabores",
-      },
-      createdAt: { type: Date, default: Date.now() },
-    },
-  ],
+  // enderecos: [
+  //   {
+  //     rua: { type: String, required: true },
+  //     numero: { type: Number, required: true },
+  //     complemento: { type: String },
+  //     CEP: {type: String, required: true},
+  //     createdAt: { type: Date, required: true, default: Date.now() }, // Armazena a data de criação dos ende... para futuras auditorias
+  //   },
+  // ],
+  // sabores_fav: [
+  //   {
+  //     _id: {
+  //       type: mongoose.Schema.Types.ObjectId,
+  //       unique: true,
+  //       ref: "sabores",
+  //     },
+  //     createdAt: { type: Date, default: Date.now() },
+  //   },
+  // ],
   createdAt: { type: Date, default: Date.now() },
   admin: { type: Boolean, required: true, default: false}, //false será valor padrão
 });
